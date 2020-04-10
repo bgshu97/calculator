65dd15c677b46406f67f0947c252d41afc26472a
Ben Shu <Ben.Shu@neudesic.com>
2020-04-10 10:49:32 -0700

    Major changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import HelloWorld from './HelloWorld';
 
 const App = () => {
   return (<div className="App">
-    <HelloWorld name="Jim" />
-    <HelloWorld name="Sally" />
+    <HelloWorld name="1" />
   </div>
   );
 };
--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -4,22 +4,77 @@ import './HelloWorld.css';
 class HelloWorld extends Component {
     constructor(props) {
         super(props);
-        this.state = { greeting: 'Hello' };
-        this.frenchify = this.frenchify.bind(this);
+        this.state = { screen: 'Welcome to the Calculator App!' };
     }
 
     render() {
         return (
-            <div className="HelloWorld">
-                {this.state.greeting} {this.props.name}!
-                <br/>
-                <button onClick={this.frenchify}>Frenchify!</button>
-            </div>
+            <React.Fragment>
+                <div className="HelloWorld">
+                    {this.state.screen}
+                    <br />
+                    <button onClick={() => this.inputNumber(0)}>0</button>
+                    <button onClick={() => this.inputNumber(1)}>1</button>
+                    <button onClick={() => this.inputNumber(2)}>2</button>
+                    <button onClick={() => this.inputNumber(3)}>3</button>
+                    <button onClick={() => this.inputNumber(4)}>4</button>
+                    <button onClick={() => this.inputNumber(5)}>5</button>
+                    <button onClick={() => this.inputNumber(6)}>6</button>
+                    <button onClick={() => this.inputNumber(7)}>7</button>
+                    <button onClick={() => this.inputNumber(8)}>8</button>
+                    <button onClick={() => this.inputNumber(9)}>9</button>
+                    <button onClick={() => this.inputCommand('+')}>+</button>
+                    <button onClick={() => this.inputCommand('-')}>-</button>
+                    <button onClick={() => this.inputCommand('*')}>*</button>
+                    <button onClick={() => this.inputCommand('/')}>/</button>
+                    <button onClick={() => this.executeCommand()}>=</button>
+                    <button onClick={() => this.clearCommand()}>c</button>
+                    <br />
+                    {this.state.result}
+                </div>
+            </React.Fragment>
         );
     }
 
-    frenchify() {
-        this.setState({ greeting: 'Bonjour' });
+    inputNumber(number) {
+        if (this.state.command) {
+            if (this.state.param2) {
+                this.setState({ param2: "" + this.state.param2 + number });
+            } else {
+                this.setState({ param2: number });
+            }
+        } else {
+            if (this.state.param1) {
+                this.setState({ param1: "" + this.state.param1 + number });
+            } else {
+                this.setState({ param1: number });
+            }
+        }
+    }
+
+    inputCommand(command) {
+        this.setState({ command: command })
+    }
+
+    executeCommand() {
+        if (this.state.command === '+') {
+            this.setState({ result: parseInt(this.state.param1) + parseInt(this.state.param2) });
+        }
+        else if (this.state.command === '-') {
+            this.setState({ result: parseInt(this.state.param1) - parseInt(this.state.param2) });
+        }
+        else if (this.state.command === '*') {
+            this.setState({ result: parseInt(this.state.param1) * parseInt(this.state.param2) });
+        }
+        else if (this.state.command === '/') {
+            this.setState({ result: parseInt(this.state.param1) / parseInt(this.state.param2) });
+        }
+    }
+
+    clearCommand() {
+        console.log(this.state);
+        this.setState({ command: '' });
+        console.log(this.state);
     }
 }
 
